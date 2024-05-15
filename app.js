de0454a21835dff67c01a63584a74bca65f664ad
Juanma877 <espasandin.j@gmail.com>
2024-05-15 17:48:29 -0300

    Utilizacion de API para el index

--- app.js
+++ app.js
@@ -5,3 +5,56 @@ function unCheckMenu() {
 }
 
 window.addEventListener('resize', unCheckMenu );
+
+
+// Fetch de api
+
+fetch('https://audius-discovery-4.theblueprint.xyz/v1/tracks/trending')
+    .then(Response => Response.json())
+    .then(data => {
+        const firstTenAlbums = data.data.slice(0,12);
+        const albumsCardHTML = firstTenAlbums.map(data => 
+        
+        `
+        <div class="card">
+        <img src="${data.artwork['150x150']}">
+        <div class="titulo">
+        <h3>${data.title}<br>${data.genre}</h3>
+        </div>
+        </div>
+        
+
+
+        `).join('');
+        
+
+        document.getElementById('albums').innerHTML = albumsCardHTML;
+    })
+    .catch(error=>console.log('error', error))
+    
+
+
+    fetch('https://audius-discovery-4.theblueprint.xyz/v1/tracks/trending')
+    .then(Response => Response.json())
+    .then(data => {
+        const firstTenNovedades = data.data.slice(13,24);
+        const novedadesCardHTML = firstTenNovedades.map(data => 
+        
+        `
+        <div class="card2">
+        <img src="${data.artwork['150x150']}">
+        <div class="titulo2">
+        <h3>${data.title}<br>${data.genre}</h3>
+        </div>
+        </div>
+        
+        
+
+
+        `).join('');
+        
+
+        document.getElementById('novedades').innerHTML = novedadesCardHTML;
+    })
+    .catch(error=>console.log('error', error))
+    
