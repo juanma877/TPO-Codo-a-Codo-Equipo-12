9eb49b0c05ef327ece6970a426d0d89cf97e214a
afiorentino73 <afiorentino@gmail.com>
2024-05-26 04:54:56 -0300

    Prueba Rebase

--- app.js
+++ app.js
@@ -5,89 +5,56 @@ function unCheckMenu() {
 }
 
 window.addEventListener('resize', unCheckMenu );
-/* Function para desactivar check del menu-toggle */
 
 
-/* Validacion del formulario */
-function validarFormulario() {
-    valor=document.getElementById("nroTelefono").value;
-    
-        if( isNaN(valor) ) 
-        {
-            document.getElementById('error-nroTelefono').innerHTML = "Ingrese numeros!";
-            /*limpiar();*/
-            alert("Ingrese Numeros");
-            return false;
-        }
-        else    
-        {
-            if( valor < 0 || valor > 9999999999 || valor ==="") 
-            {
-                document.getElementById('error-nroTelefono').innerHTML = "Dato incorrecto!";
-                /*limpiar();*/
-                alert("Ingrese Numeros");
-                return false;
-            }
-        }
-       /* return true;          
-    /*}
-    
-    function validacion() {*/
-    valor = document.getElementById("firstName").value;
-    document.getElementById('error-firstName').innerHTML = "";
+// Fetch de api
+
+fetch('https://audius-discovery-4.theblueprint.xyz/v1/tracks/trending')
+    .then(Response => Response.json())
+    .then(data => {
+        const firstTenAlbums = data.data.slice(0,12);
+        const albumsCardHTML = firstTenAlbums.map(data => 
         
-        if( valor == null || valor.length == 0) 
-        {
-            document.getElementById('error-firstName').innerHTML = "Tenes que completar el Nombre!";
-            elemento = document.getElementById('firstName');
-            elemento.focus();
-            
-            return false;
-        }
-        console.log("Nombre",firstName.value,"Apellido",lastName.value,"Tel",nroTelefono.value,"Genero",genMusical.value);
-        alert("Formulario Enviado!");
+        `
+        <div class="card">
+        <img src="${data.artwork['150x150']}">
+        <div class="titulo">
+        <h3>${data.title}<br>${data.genre}</h3>
+        </div>
+        </div>
         
-        /*return true;  */
+
+
+        `).join('');
         
-    }
 
+        document.getElementById('albums').innerHTML = albumsCardHTML;
+    })
+    .catch(error=>console.log('error', error))
     
 
-/* tomado del video 10 cAC pero NO esta funcionando 
-const firstName = document.querySelector("#firstName");
-const lastName = document.querySelector("#lastName");
-const nroTelefono = document.querySelector("#ntoTelefono");
-const email = document.querySelector("#email");
 
-if(firstName && lastName && nroTelefono && email) {
-    formRegister.addEventListener('submit', validarFormulario);
-    } else {
-    console.log("error no se puede enviar")
-    }
+    fetch('https://audius-discovery-4.theblueprint.xyz/v1/tracks/trending')
+    .then(Response => Response.json())
+    .then(data => {
+        const firstTenNovedades = data.data.slice(13,24);
+        const novedadesCardHTML = firstTenNovedades.map(data => 
+        
+        `
+        <div class="card2">
+        <img src="${data.artwork['150x150']}">
+        <div class="titulo2">
+        <h3>${data.title}<br>${data.genre}</h3>
+        </div>
+        </div>
+        
+        
 
-function validarFormulario(event) {
-    event.preventDefault();
 
-    let validation = true;
+        `).join('');
+        
 
-    if (firstName.value ==="") {
-        firstName.classList.add("error");
-        errorfirstName.textContent = "El nombre no puede estar vacio"
-        validation = false
-    } else {
-        firstName.classList.remove("error");
-        errorfirstName.textContent = ""
-    }
-    }
-
-    if (validation) {
-    console.log ("Datos validos")
-    } else {
-    console.log("error no se puede enviar")
-    }
-    firstName.addEventListener("input",()=>
-        if( firstName.value != "")
+        document.getElementById('novedades').innerHTML = novedadesCardHTML;
+    })
+    .catch(error=>console.log('error', error))
     
-    
-    )
-*/ 
